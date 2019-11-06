5b1384b8c60dfdf5b329e5e6ef862bb7deaf77dc
lerdeljan17 <lerdeljan17@raf.rs>
2019-11-06 19:00:45 +0100

    init

---
+++ FileStorageApp/src/main/java/raf/rs/FileStorageApp/App.java
@@ -0,0 +1,16 @@
+package raf.rs.FileStorageApp;
+
+import raf.rs.FileStorageLocalImpl.model.FileStorageLocal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
